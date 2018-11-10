62bd754fd0c5a3f67b3e2a2fc421bd1f1c7b577a
Hannes Hochreiner <hannes@hochreiner.net>
2018-11-10 08:50:26 +0100

    added device iteration

--- driver/ism-gateway/src/main.rs
+++ driver/ism-gateway/src/main.rs
@@ -4,4 +4,22 @@ fn main() {
     let version = libusb::version();
 
     println!("Using libusb version {}.{}.{}.{}", version.major(), version.minor(), version.micro(), version.nano());
+    
+    let context = libusb::Context::new().unwrap();
+
+    for device in context.devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+        let is_custom = if device_desc.vendor_id() == 0x0483 && device_desc.product_id() == 0x5740 {
+            true
+        } else {
+            false
+        };
+
+        println!("Bus {:03} Device {:03} ID {:04x}:{:04x} {}",
+            device.bus_number(),
+            device.address(),
+            device_desc.vendor_id(),
+            device_desc.product_id(),
+            is_custom);
+    }
 }
